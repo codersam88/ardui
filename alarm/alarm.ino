79cc92283c8526cb2071671d55028bcf7ecfd211
sampath <sampath.skt@gmail.com>
2015-04-14 22:40:39 +0530

    done changing height program

--- alarm/alarm.ino
+++ alarm/alarm.ino
@@ -1,20 +1,18 @@
-long doorfar=25;
+long doorfar = 25;
 long alarmfar = 5;
 long difference;
 void setup() {
-  pinMode (2,OUTPUT);//attach pin 2 to vcc
-  pinMode (5,OUTPUT);//attach pin 5 to GND
+  pinMode (2, OUTPUT); //attach pin 2 to vcc
+  pinMode (5, OUTPUT); //attach pin 5 to GND
   // initialize serial communication:
   Serial.begin(9600);
-  
-  
-pinMode(9, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(2, HIGH);
-  
+
   long duration, distin;
 
   pinMode(3, OUTPUT);// attach pin 3 to Trig
@@ -26,30 +24,30 @@ void loop()
 
   pinMode (4, INPUT);//attach pin 4 to Echo
   duration = pulseIn(4, HIGH);
-  
- distin = microsecondsToInches(duration);
-  difference = doorfar-distin;
-  
-  
-  
+
+  distin = microsecondsToInches(duration);
+  difference = doorfar - distin;
+
+
+
   Serial.print("The difference is  ");
   Serial.print(difference);
   Serial.print("in, ");
   Serial.println();
-  
-  if(abs(difference) > alarmfar)
+
+  if (abs(difference) > alarmfar)
   {
-    
-    
-  
-  beep(2000);
-  delay(1000);
 
-  beep(2000);
-  delay(1000);
 
-  beep(2000);
-  delay(1000);
+
+    beep(2000);
+    delay(1000);
+
+    beep(2000);
+    delay(1000);
+
+    beep(2000);
+    delay(1000);
 
   }
   delay(1000);
@@ -65,10 +63,10 @@ long microsecondsToCentimeters(long microseconds)
   return microseconds / 29 / 2;
 }
 
-void beep(unsigned char delayms){
+void beep(unsigned char delayms) {
   analogWrite(9, 20);      // Almost any value can be used except 0 and 255
-                           // experiment to get the best tone
+  // experiment to get the best tone
   delay(delayms);          // wait for a delayms ms
   analogWrite(9, 0);       // 0 turns it off
-  delay(delayms);          // wait for a delayms ms   
-} 
+  delay(delayms);          // wait for a delayms ms
+}
--- distance_only/distance_only.ino
+++ distance_only/distance_only.ino
@@ -1,49 +1,64 @@
-long dist=0;
+long duration, heightin , heightcm, distin, distcm;
 
-void setup() {
-  pinMode (2,OUTPUT);//attach pin 2 to vcc
-  pinMode (5,OUTPUT);//attach pin 5 to GND
-  // initialize serial communication:
-  Serial.begin(9600);
-}
-
-void loop()
+void getDist()
 {
-  digitalWrite(2, HIGH);
   
-  long duration, inches, cm,distin,distcm;
-
-  pinMode(3, OUTPUT);// attach pin 3 to Trig
   digitalWrite(3, LOW);
   delayMicroseconds(2);
   digitalWrite(3, HIGH);
   delayMicroseconds(5);
   digitalWrite(3, LOW);
 
-  pinMode (4, INPUT);//attach pin 4 to Echo
   duration = pulseIn(4, HIGH);
-  
-  distin = 0;
-  distcm = 0;
 
-  inches = microsecondsToInches(duration);
-  cm = microsecondsToCentimeters(duration);
-  
-  
-  inches = distin-inches;
-  cm= distcm-cm;
-  
+  distin = microsecondsToInches(duration);
+  distcm = microsecondsToCentimeters(duration);
+  delay(1000);//for distance to get registered
+    
   Serial.print("The dist is ");
   Serial.print(distin);
   Serial.print("in, ");
   Serial.print(distcm);
   Serial.print("cm");
   Serial.println();
- 
+}
+
+void setup() {
+  pinMode (2, OUTPUT); //attach pin 2 to vcc
+  pinMode(3, OUTPUT);// attach pin 3 to Trig
+  pinMode (4, INPUT);//attach pin 4 to Echo
+  pinMode (5, OUTPUT); //attach pin 5 to GND
+  pinMode (13, OUTPUT);//led pin
+  // initialize serial communication:
+  Serial.begin(9600);
+  digitalWrite(2, HIGH);
+  getDist();
+  delay(5000);//giving time to kids to clear the place
+  getDist();
+  digitalWrite(13, HIGH);
+}
+
+void loop()
+{
+  digitalWrite(3, LOW);
+  delayMicroseconds(2);
+  digitalWrite(3, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(3, LOW);
+
+  duration = pulseIn(4, HIGH);
+
+  heightin = microsecondsToInches(duration);
+  heightcm = microsecondsToCentimeters(duration);//b4 subtracting
+
+  heightin = distin - heightin;
+  heightcm = distcm - heightcm;
+
+
   Serial.print("Your height is ");
-  Serial.print(inches);
+  Serial.print(heightin);
   Serial.print("in, ");
-  Serial.print(cm);
+  Serial.print(heightcm);
   Serial.print("cm");
   Serial.println();
   delay(1000);
@@ -58,3 +73,5 @@ long microsecondsToCentimeters(long microseconds)
 {
   return microseconds / 29 / 2;
 }
+
+
